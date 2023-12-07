170d3de3836b3dca79849388b4ee3ae2a48d3fe2
countkhalil <152437499+countkhalil@users.noreply.github.com>
2023-12-06 18:57:26 -0700

    Collections Notes
    Notes About Collections Types

--- week4Assignments/src/week4Notes/Week4Collections.java
+++ week4Assignments/src/week4Notes/Week4Collections.java
@@ -1,10 +1,76 @@
 package week4Notes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Week4Collections {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * including ArrayLsit theres LinkedList & Vector
+		 */
+List<String> pickMe = new ArrayList<String>();
+pickMe.add("Eenie");
+pickMe.add("Meenie");
+pickMe.add("Minie");
+pickMe.add("Minie");
+pickMe.add("Moe");
+pickMe.add(null);
 
-	}
-
+for (String picks : pickMe) {
+	System.out.println(picks);
 }
+System.out.println(pickMe.get(2));
+
+	///This is a Set>>
+/*
+ * Includes HAshSet, LinkedHashSet, TreeSet>>
+ */
+Set<String> sing = new HashSet<String>();
+sing.add("Do");
+sing.add("Re");
+//Will only put (1) Re since duplicates not allowed
+sing.add("Re");
+sing.add("Me");
+sing.add("Fa");
+if (sing.contains("Fa")) {
+		sing.remove("Me");
+	}
+	for (String song: sing) {
+		System.out.println(song);
+	// Will probably print out of order
+	}
+	
+// This is a Map-- Includes: HashMap, LinkedHashMap, TreeMap, Hashtable >>>
+	/*
+	 * 1st value= Key, 2nd Value is Value 
+	 * Dictionary Ex: Word=Key; Definition=Value
+	 */
+	Map <Integer, String> racePlacements = new HashMap <Integer, String>();
+racePlacements.put(1, "Tyreke");
+racePlacements.put(2, "Lee");
+racePlacements.put(3, "Timmy");
+racePlacements.put(4, "Diego");
+
+System.out.println(racePlacements.get(1));
+
+Set<Integer> racerKeys = racePlacements.keySet();
+for (Integer key: racerKeys) {
+	System.out.println(key + " : " + racePlacements.get(key));
+}
+ 
+Map<String,String> dictionary = new HashMap<String, String>();
+dictionary.put("zebastian", "A Great Looking Man");
+dictionary.put("Taylor", "A Bum");
+
+
+
+
+
+
+}}
